be5620b2791652883b822a14c9e3bf4be7059a47
Raghu Betina <raghu@firstdraft.com>
2017-05-09 06:54:57 -0500

    Add seed data script

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) do |repo_name|
 end
 
 gem "starter_generators", :github => "raghubetina/starter_generators"
+gem "faker"
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.create(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+board = Board.new
+board.title = "Books to read"
+board.save
+
+7.times do
+  list = List.new
+  list.board_id = board.id
+  list.title = Faker::Book.genre
+  list.save
+
+  rand(10).times do
+    card = Card.new
+    card.list_id = list.id
+    card.title = Faker::Book.title
+    card.description = "by #{Faker::Book.author}, from #{Faker::Book.publisher}"
+    card.save
+  end
+end
+
+
+board = Board.new
+board.title = "Great companies"
+board.save
+
+7.times do
+  list = List.new
+  list.board_id = board.id
+  list.title = Faker::Company.name
+  list.save
+
+  rand(10).times do
+    card = Card.new
+    card.list_id = list.id
+    card.title = Faker::Company.buzzword
+    card.description = Faker::Company.catch_phrase
+    card.save
+  end
+end
